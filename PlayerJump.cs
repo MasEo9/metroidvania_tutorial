c5181da7a24bf5156b0e25114eb43ea82312c6ae
Andrew Mason <ajmason09@gmail.com>
2021-08-02 21:53:09 -0400

    jump feedback based on length of button press

--- PlayerJump.cs
+++ PlayerJump.cs
@@ -6,30 +6,60 @@ public class PlayerJump : MonoBehaviour
 {
     //force, apply force, 1x 
 
-    [Header("Public Vars")]
+    [Header("Jump Details")]
     public float jumpForce;
-    public bool grounded;
-    private Rigidbody2D rb;
+    public float jumpTime;
+    private float jumpTimeCounter;
+    private bool stoppedJumping;
 
-    [Header("Private Vars")]
+
+    [Header("Ground Details")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float radOCircle;
     [SerializeField] private LayerMask whatIsGround;
+    public bool grounded;
+
+
+    [Header("Components")]
+    private Rigidbody2D rb;
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumpTimeCounter = jumpTime;
 
     }
 
     private void Update()
     {
         grounded = Physics2D.OverlapCircle(groundCheck.position, radOCircle, whatIsGround);
+
+        if (grounded)
+        {
+            jumpTimeCounter = jumpTime;
+        }
+
+
         if (Input.GetButtonDown("Jump") && grounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            stoppedJumping = false;
         }
+
+        if(Input.GetButton("Jump") && !stoppedJumping && jumpTimeCounter > 0)
+        {
+
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpTimeCounter -= Time.deltaTime;
+        }
+
+        if(Input.GetButtonUp("Jump"))
+        {
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
+        }
+
     }
 
     private void OnDrawGizmos()
